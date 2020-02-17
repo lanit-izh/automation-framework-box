caf2069103fcdf185b728a217a41bffe3fa994fe
danilishe <yraiy-45674>
2020-02-17 15:39:46 +0300

    execute methods must be deprecated for clarity and less abstraction

--- src/test/java/steps/CommonStepsLibrary.java
+++ src/test/java/steps/CommonStepsLibrary.java
@@ -77,11 +77,19 @@ public final class CommonStepsLibrary extends BaseSteps {
         }
     }
 
+    /**
+     * @deprecated предлагаю удалить и не использовать этот и подобные методы, так как они создают дополнительный
+     * слой абстракции который сложно понимать и поддерживать
+     */
     @И("выполнить действие {string}")
     public void executeMethod(String methodName) {
         executeMethodByName(getSearchContext(), methodName);
     }
 
+    /**
+     * @deprecated предлагаю удалить и не использовать этот и подобные методы, так как они создают дополнительный
+     * слой абстракции который сложно понимать и поддерживать
+     */
     @И("выполнить действие {string}, c аргументами {string}")
     public void executeMethod(String methodName, String arg) {
         executeMethodByName(getSearchContext(), methodName, arg);
